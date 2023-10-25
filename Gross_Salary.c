96d6c6cdc4711cf00d763a4fc7064711a5bd8e7c
tripurad <147791775+tripurad@users.noreply.github.com>
2023-10-25 16:12:38 +0530

    #include<stdio.h> int main() {     int bs;     float gs,da,hra;     scanf("%d",&bs);     if(bs<=10000)     {         da=0.80*bs;         hra=0.20*bs;         gs=bs+da+hra;         printf("%.2f",gs);     }     else if(bs<=20000)     {         da=0.90*bs;         hra=0.25*bs;         gs=bs+da+hra;         printf("%.2f",gs);     }     else     {         da=0.95*bs;         hra=0.30*bs;         gs=bs+da+hra;         printf("%.2f",gs);     } }

---
+++ Gross_Salary.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int bs;
+    float gs,da,hra;
+    scanf("%d",&bs);
+    if(bs<=10000)
+    {
+        da=0.80*bs;
+        hra=0.20*bs;
+        gs=bs+da+hra;
+        printf("%.2f",gs);
+    }
+    else if(bs<=20000)
+    {
+        da=0.90*bs;
+        hra=0.25*bs;
+        gs=bs+da+hra;
+        printf("%.2f",gs);
+    }
+    else
+    {
+        da=0.95*bs;
+        hra=0.30*bs;
+        gs=bs+da+hra;
+        printf("%.2f",gs);
+    }
+}

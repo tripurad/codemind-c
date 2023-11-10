dd613b36457f312f5b05e7b9eca78218cf121e20
tripurad <147791775+tripurad@users.noreply.github.com>
2023-11-10 11:54:52 +0530

    #include<stdio.h> int main() { 	int i,n,a; 	scanf("%d",&n); 	for(i=1;i<=n;i++) 	{ 	   a=3*i; 	   printf("%d ",a); 	} }

--- First_N_multiples_of_3.c
+++ First_N_multiples_of_3.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,m;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        m=3*i;
-        printf("%d ",m);
-    }
+	int i,n,a;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	   a=3*i;
+	   printf("%d ",a);
+	}
 }

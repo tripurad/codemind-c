740348946ccfc25d3d79ee1342a303d244ba86ed
tripurad <147791775+tripurad@users.noreply.github.com>
2023-11-29 22:45:41 +0530

    #include<stdio.h> int main() {     int i,n,n1=0,n2=1,n3;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         n3=n1+n2;         printf("%d ",n1);         n1=n2;         n2=n3;     } }

--- fibonacci.c
+++ fibonacci.c
@@ -3,11 +3,10 @@ int main()
 {
     int i,n,n1=0,n2=1,n3;
     scanf("%d",&n);
-    printf("%d %d ",n1,n2);
-    for(i=2;i<n;i++)
+    for(i=1;i<=n;i++)
     {
         n3=n1+n2;
-        printf("%d ",n3);
+        printf("%d ",n1);
         n1=n2;
         n2=n3;
     }

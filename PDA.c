264f328b989c4b489d90ebcae7c060c0e686f6f8
tripurad <147791775+tripurad@users.noreply.github.com>
2023-12-03 13:47:52 +0530

    #include<stdio.h> int main() {     int i,n,sum=0,fact;     scanf("%d",&n);     for(i=1;i<n;i++)     {         if(n%i==0)         sum=sum+i;     }     if(sum==n)     printf("PERFECT");     else if(sum<n)     printf("DEFICIENT");     else     printf("ABUNDANT"); }

---
+++ PDA.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=0,fact;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        sum=sum+i;
+    }
+    if(sum==n)
+    printf("PERFECT");
+    else if(sum<n)
+    printf("DEFICIENT");
+    else
+    printf("ABUNDANT");
+}

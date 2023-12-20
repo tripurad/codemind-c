b0fbc29159ab1aeaccb4f0617a6cb625f1541b92
tripurad <147791775+tripurad@users.noreply.github.com>
2023-12-20 21:20:22 +0530

    #include<stdio.h> int main() {     int i,j,n;     char ch='A';     scanf("%d",&n);     for(i=1;i<=n;i++)     {         ch='A';         for(j=1;j<=i;j++)         {             printf("%c ",ch++);         }         printf(" ");     }     return 0; }

---
+++ Basic_Pattern_2.3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    char ch='A';
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        ch='A';
+        for(j=1;j<=i;j++)
+        {
+            printf("%c ",ch++);
+        }
+        printf("
+");
+    }
+    return 0;
+}

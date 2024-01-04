705dc2c935a24e1a569de27f6346614b24573e8c
tripurad <147791775+tripurad@users.noreply.github.com>
2024-01-04 18:31:08 +0530

    #include<stdio.h> int main() {     int size;     scanf("%d",&size);     int inputArray[size];     int oddArray[size],evenArray[size];     int oddCount=0,evenCount=0;     for(int i=0;i<size;++i)     {         scanf("%d",&inputArray[i]);         if(inputArray[i]%2==0)         {             evenArray[evenCount++]=inputArray[i];         }         else         {             oddArray[oddCount++]=inputArray[i];         }     }     for(int i=0;i<evenCount;++i)     {         printf("%d ",evenArray[i]);     }     for(int i=0;i<oddCount;++i)     {         printf("%d ",oddArray[i]);     }     return 0; }

---
+++ Even_elements_followed_by_Odd_elements.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int size;
+    scanf("%d",&size);
+    int inputArray[size];
+    int oddArray[size],evenArray[size];
+    int oddCount=0,evenCount=0;
+    for(int i=0;i<size;++i)
+    {
+        scanf("%d",&inputArray[i]);
+        if(inputArray[i]%2==0)
+        {
+            evenArray[evenCount++]=inputArray[i];
+        }
+        else
+        {
+            oddArray[oddCount++]=inputArray[i];
+        }
+    }
+    for(int i=0;i<evenCount;++i)
+    {
+        printf("%d ",evenArray[i]);
+    }
+    for(int i=0;i<oddCount;++i)
+    {
+        printf("%d ",oddArray[i]);
+    }
+    return 0;
+}
